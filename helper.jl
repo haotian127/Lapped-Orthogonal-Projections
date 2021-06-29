ed1ae452e7f66c18befb15e805022bd527968c59
haotian127 <htsli@ucdavis.edu>
2021-06-28 17:10:25 -0700

    try out a path case: AAPL

--- helper.jl
+++ helper.jl
@@ -7,7 +7,7 @@ function approx_errors(DVEC::Vector{Vector{Float64}}; frac::Float64 = 0.50, ϵ =
          (:dashdot, :pink), (:solid, :gray), (:solid, :green), (:solid, :blue),
          (:solid, :black), (:solid, :orange)]
     for i = 1:length(DVEC)
-        if i in [1, 4, 5]
+        if i in [1, 4, 5, 6, 7, 8]
             continue
         end
         dvec = DVEC[i]
@@ -19,4 +19,42 @@ function approx_errors(DVEC::Vector{Vector{Float64}}; frac::Float64 = 0.50, ϵ =
         plot!(frac*(0:(p-1))/(p-1), er[1:p], yaxis=:log, xlims = (0.,frac),
                 label = T[i], line = L[i], linewidth = 2, grid = false)
     end
+end
+
+
+function approx_curves_now(G_Sig, GP, GP_dual, VM_NGWP, LP_NGWP)
+    ############# VM_NGWP
+    dmatrix_VM = ngwp_analysis(G_Sig, VM_NGWP)
+    dvec_vm_ngwp, BS_vm_ngwp = ngwp_bestbasis(dmatrix_VM, GP_dual)
+    ############# LP_NGWP
+    dmatrix_LP = ngwp_analysis(G_Sig, LP_NGWP)
+    dvec_lp_ngwp, BS_lp_ngwp = ngwp_bestbasis(dmatrix_LP, GP_dual)
+    ############# Laplacian L
+    dvec_Laplacian = 𝚽' * G_Sig.f
+    ############# HGLET
+    GP = partition_tree_fiedler(G_Sig)
+    dmatrixH, _, _ = HGLET_Analysis_All(G_Sig, GP)
+    dvec_hglet, BS_hglet, _ = HGLET_GHWT_BestBasis(GP, dmatrixH = dmatrixH, costfun = 1)
+    ############# LP-HGLET
+    dmatrixlpH, _ = LPHGLET_Analysis_All(G_Sig, GP; ϵ = 0.3)
+    dvec_lphglet, BS_lphglet, _ = HGLET_GHWT_BestBasis(GP, dmatrixH = dmatrixlpH, costfun = 1)
+    ############# GHWT dictionaries
+    dmatrix = ghwt_analysis!(G_Sig, GP = GP)
+    ############# Haar
+    BS_haar = bs_haar(GP)
+    dvec_haar = dmatrix2dvec(dmatrix, GP, BS_haar)
+    ############# Walsh
+    BS_walsh = bs_walsh(GP)
+    dvec_walsh = dmatrix2dvec(dmatrix, GP, BS_walsh)
+    ############# GHWT_c2f
+    dvec_c2f, BS_c2f = ghwt_c2f_bestbasis(dmatrix, GP)
+    ############# GHWT_f2c
+    dvec_f2c, BS_f2c = ghwt_f2c_bestbasis(dmatrix, GP)
+    ############# eGHWT
+    dvec_eghwt, BS_eghwt = ghwt_tf_bestbasis(dmatrix, GP)
+    DVEC = [dvec_Laplacian[:], dvec_hglet[:], dvec_lphglet[:], dvec_haar[:], dvec_walsh[:],
+            dvec_c2f[:], dvec_f2c[:], dvec_eghwt[:], dvec_vm_ngwp[:], dvec_lp_ngwp[:]]
+    approx_errors(DVEC; frac = 0.5);
+    plt = plot!(xguidefontsize = 14, yguidefontsize = 14, legendfontsize = 11)
+    return plt
 end
---
+++ path.jl
@@ -0,0 +1,105 @@
+cd(@__DIR__); include("helper.jl")
+using MultiscaleGraphSignalTransforms, Plots, LightGraphs, JLD, MarketData
+
+## Build a path graph
+N = length(f)
+G = path_graph(N)
+X = zeros(N, 2); X[:, 1] = 1:N;
+W = 1.0 * adjacency_matrix(G)
+L = laplacian_matrix(G)
+
+## eigenvectors of L(G)
+𝛌, 𝚽 = eigen(Matrix(L))
+standardize_eigenvectors!(𝚽)
+
+## Build Dual Graph
+Gstar_Sig = GraphSig(W, xy = X)
+G_Sig = GraphSig(W, xy = X)
+GP_dual = partition_tree_fiedler(Gstar_Sig; swapRegion = false)
+jmax = size(GP_dual.rs, 2) - 1  # zero-indexed
+
+@time VM_NGWP = vm_ngwp(𝚽, GP_dual)
+@time LP_NGWP = lp_ngwp(𝚽, Gstar_Sig.W, GP_dual; ϵ = 0.3)
+
+## graph signal
+start = DateTime(2020, 3, 11)
+stop = DateTime(2021, 6, 29)
+dataset = yahoo(:AAPL, YahooOpt(period1 = start, period2 = stop))
+f = values(dataset[:Volume])
+f .-= mean(f)
+G_Sig.f = reshape(f, (N, 1))
+plot(f, lw = 2, c = :black, legend = false, frame = :box)
+
+##
+############# VM_NGWP
+dmatrix_VM = ngwp_analysis(G_Sig, VM_NGWP)
+dvec_vm_ngwp, BS_vm_ngwp = ngwp_bestbasis(dmatrix_VM, GP_dual)
+############# LP_NGWP
+dmatrix_LP = ngwp_analysis(G_Sig, LP_NGWP)
+dvec_lp_ngwp, BS_lp_ngwp = ngwp_bestbasis(dmatrix_LP, GP_dual)
+############# HGLET
+GP = partition_tree_fiedler(G_Sig)
+dmatrixH, _, _ = HGLET_Analysis_All(G_Sig, GP)
+dvec_hglet, BS_hglet, _ = HGLET_GHWT_BestBasis(GP, dmatrixH = dmatrixH, costfun = 1)
+############# LP-HGLET
+dmatrixlpH, _ = LPHGLET_Analysis_All(G_Sig, GP; ϵ = 0.3)
+dvec_lphglet, BS_lphglet, _ = HGLET_GHWT_BestBasis(GP, dmatrixH = dmatrixlpH, costfun = 1)
+
+#############
+approx_curves_now(G_Sig, GP, GP_dual, VM_NGWP, LP_NGWP)
+
+
+
+# ## top VM-NGWP
+# important_idx = sortperm(dvec_vm_ngwp[:].^2; rev = true)
+# plot(layout = Plots.grid(5, 6), size = (1000, 600))
+# for i in 1:30
+#     dr, dc = BS_vm_ngwp.levlist[important_idx[i]]
+#     w = VM_NGWP[dr, dc, :]
+#     w .*= (maximum(w) > -minimum(w)) * 2 - 1
+#     j, k, l = NGWP_jkl(GP_dual, dr, dc)
+#     plot!(w, lw = 1, c = :black, legend = false, frame = :none, subplot = i,
+#           ylim = [-0.5, 0.25])
+#     # title = "ψ^($(j))_{$(k), $(l)}", titlefontsize = 6,
+# end
+# current()
+#
+# ## top LP-NGWP
+# important_idx = sortperm(dvec_lp_ngwp[:].^2; rev = true)
+# plot(layout = Plots.grid(5, 6), size = (1000, 600))
+# for i in 1:30
+#     dr, dc = BS_lp_ngwp.levlist[important_idx[i]]
+#     w = LP_NGWP[dr, dc, :]
+#     w .*= (maximum(w) > -minimum(w)) * 2 - 1
+#     j, k, l = NGWP_jkl(GP_dual, dr, dc)
+#     plot!(w, lw = 1, c = :black, legend = false, frame = :none, subplot = i,
+#           ylim = [-0.5, 0.25])
+#     # title = "ψ^($(j))_{$(k), $(l)}", titlefontsize = 6,
+# end
+# current()
+#
+## top 16 HGLET
+important_idx = sortperm(dvec_hglet[:].^2; rev = true)
+plot(layout = Plots.grid(5, 6), size = (1000, 600))
+for i in 1:30
+    dr, dc = BS_hglet.levlist[important_idx[i]]
+    w, _ = HGLET_Synthesis(reshape(spike(important_idx[i], N), (N, 1)),
+                           GP, BS_hglet, G_Sig, method = :L)
+    w .*= (maximum(w) > -minimum(w)) * 2 - 1
+    j, k, l = HGLET_jkl(GP, dr, dc)
+    plot!(w, lw = 1, c = :black, legend = false, frame = :none, subplot = i)
+end
+current()
+
+## top 16 LP-HGLET
+important_idx = sortperm(dvec_lphglet[:].^2; rev = true)
+plot(layout = Plots.grid(5, 6), size = (1000, 600))
+for i in 31:60
+    dr, dc = BS_lphglet.levlist[important_idx[i]]
+    w, _ = LPHGLET_Synthesis(reshape(spike(important_idx[i], N), (N, 1)),
+                             GP, BS_lphglet, G_Sig; method = :L, ϵ = 0.3)
+    w .*= (maximum(w) > -minimum(w)) * 2 - 1
+    j, k, l = HGLET_jkl(GP, dr, dc)
+    plot!(w, lw = 1, c = :black, legend = false, frame = :none, subplot = i - 30)
+end
+current()
